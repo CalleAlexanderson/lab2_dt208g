3459aa930889e044a411a415d7568ab3fbbd21cd
CalleAlexanderson <calle.alexanderson@telia.com>
2024-04-04 15:52:08 +0200

    skriver ut alla todos, kan tas bort, date added lagts till

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,13 +1,12 @@
 import { todoList } from "./todo";
-
-console.log("heel");
-
+// localStorage.setItem('todos', "");
 
 document.addEventListener("DOMContentLoaded", () => {
   let todoObj = new todoList();
   todoObj.loadFromLocalStorage();
 
-  
+  showTodos();
+
   const form = document.getElementById("add_form") as HTMLFormElement;
   console.log(form);
   form.onsubmit = (event) => {
@@ -15,17 +14,62 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let newTaskInput = (
       document.getElementById("task_input") as HTMLInputElement
-    ).value;
+    );
     let newPrioInput = (
       document.getElementById("prio_input") as HTMLInputElement
     ).value;
     let prio: number = parseInt(newPrioInput);
 
-    let d: boolean = todoObj.addTodo(newTaskInput, prio)
+    let d: boolean = todoObj.addTodo(newTaskInput.value, prio);
     if (d == false) {
       alert("Du måste fylla i uppgift fältet för att skapa en uppgift");
     }
 
     console.log("formulär inlämmat");
+    newTaskInput.value = "";
+    showTodos();
   };
+
+  function showTodos() {
+    const todoDiv = document.getElementById("list_div") as HTMLDivElement;
+    todoDiv.innerHTML = "";
+    let todoArr = todoObj.getTodos();
+
+    for (let index = 0; index < todoArr.length; index++) {
+      let todoArticle = document.createElement("article") as HTMLElement;
+
+      let pTask = document.createElement("p") as HTMLParagraphElement;
+      pTask.innerHTML = todoArr[index].task;
+
+      let imgComplete = document.createElement("img") as HTMLImageElement;
+      if (todoArr[index].completed == true) {
+        imgComplete.src = "";
+      } else {
+        imgComplete.src = "";
+      }
+
+      let pDate = document.createElement("p") as HTMLParagraphElement;
+      pDate.innerHTML = todoArr[index].date;
+
+      let pPrio = document.createElement("p") as HTMLParagraphElement;
+      pPrio.innerHTML = todoArr[index].priority.toString();
+
+      let btnRemove = document.createElement("button") as HTMLButtonElement;
+      btnRemove.innerHTML = "Ta bort uppgift";
+      btnRemove.addEventListener("click", () => {
+        if (btnRemove.parentElement != null) {
+          btnRemove.parentElement.remove();
+          todoObj.removeTodo(index);
+          showTodos();
+        }
+      });
+
+      todoArticle.appendChild(pTask);
+      todoArticle.appendChild(pDate);
+      todoArticle.appendChild(imgComplete);
+      todoArticle.appendChild(pPrio);
+      todoArticle.appendChild(btnRemove);
+      todoDiv.appendChild(todoArticle);
+    }
+  }
 });
--- src/ts/todo.ts
+++ src/ts/todo.ts
@@ -2,21 +2,26 @@ interface todo {
   task: string;
   completed: boolean;
   priority: number;
+  date: string;
 }
 
 export class todoList implements todo {
   task: string;
   completed: boolean;
   priority: number;
+  date: string;
   todos: todo[] = [];
 
   // skapar nya todo object, completed sätts alltid till false när objektet skapas
   addTodo(taskAdd: string, priorityAdd: number): boolean {
     if (taskAdd != "") {
+      let dateAdded = new Date();
+      console.log(dateAdded);
       let newTodo: todo = {
         task: taskAdd,
         completed: false,
         priority: priorityAdd,
+        date: dateAdded.toISOString().slice(0, 10) //metod för date konversion hittade jag här: https://stackoverflow.com/questions/23593052/format-javascript-date-as-yyyy-mm-dd
       };
       this.todos.push(newTodo);
       console.log(this.todos);
@@ -31,7 +36,12 @@ export class todoList implements todo {
   markTodoCompleted(todoIndexMark: number): void {}
 
   // en funktion som tar in ett nummer(index) och tar bort det indexet från todos array
-  removeTodo(todoIndex: number): void {}
+  removeTodo(todoIndex: number): void {
+    console.log(todoIndex);
+    this.todos.splice(todoIndex, 1);
+    console.log(this.todos);
+    this.saveToLocalStorage();
+  }
 
   getTodos(): todo[] {
     return this.todos;
@@ -46,7 +56,7 @@ export class todoList implements todo {
 
   // hämtar todos arrayen från local storage
   loadFromLocalStorage(): void {
-    if (localStorage.getItem("todos") != null) {
+    if (localStorage.getItem("todos") != null && localStorage.getItem("todos") != "") {
       const storedTodos: any = localStorage.getItem("todos");
       this.todos = JSON.parse(storedTodos);
       console.log(this.todos);
